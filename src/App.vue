947389b27e3bb72dbf7fb5dd50ace19cd1417328
34342343 <vzeldak07@gmail.com>
2024-08-15 23:20:28 +0300

    feat: add transaction list with delete functionality

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <script>
 import Header from "./components/Header.vue";
 import Balance from "./components/Balance.vue";
+import IncomeExpenses from "./components/IncomeExpenses.vue";
+import TransactionList from "./components/TransactionList.vue";
 export default {
   components: {
     Header,
     Balance,
+    IncomeExpenses,
   },
 };
 </script>
 
 <template>
-    <Header />
-    <div class="container">
-        <Balance />
-    </div>
+  <Header />
+  <div class="container">
+    <Balance />
+    <IncomeExpenses />
+    <TransitionList />
+  </div>
 </template>
